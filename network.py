ba3644d46e87e7d80b7646e7bb484cf141d69ebe
Ted <herring@pdx.edu>
2019-11-02 19:52:23 -0700

    first commit

--- network.py
+++ network.py
@@ -6,8 +6,10 @@ class Network:
         self.server = "10.0.1.8"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
@@ -23,6 +25,3 @@ class Network:
         except socket.error as e:
             print(e)
 
-n = Network()
-print(n.send("hello"))
-print(n.send("working"))
