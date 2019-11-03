742edf4735b24aef2f07126a74cceef0bf999cf8
Ted <herring@pdx.edu>
2019-11-03 14:47:35 -0800

    first commit

--- client.py
+++ client.py
@@ -137,5 +137,26 @@ def main():
 
             redrawWindow(win, game, player)
 
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
 
-main()
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("console", 24)
+        text = font.render("Click to Play!", 1, (255,0,0))
+        win.blit(text, (100,200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+    main()
+
+while True:
+    menu_screen()
+
