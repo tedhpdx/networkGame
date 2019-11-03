2e0462993fc2ce03d67e586de8a789c09ea8c8fb
Ted <herring@pdx.edu>
2019-11-03 10:38:58 -0800

    first commit

--- network.py
+++ network.py
@@ -1,27 +1,29 @@
 import socket
+import pickle
+
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "10.0.1.8"
-        self.port = 5555
+        self.port = 5550
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
 
